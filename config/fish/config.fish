d076a0fdc4db3d6b5b43e39cef21afa38e91bfac
Agustin Chiappe Berrini <agustin@chiappeberrini.me>
2022-04-09 08:25:29 -0700

    view markdowns and webpages

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+alias vw='rdrview -B lynx'
+alias vm='glow -p'
 set -g theme_color_scheme nord
 set -g fish_prompt_pwd_dir_length 0
 setenv EDITOR vim
