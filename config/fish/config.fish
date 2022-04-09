556480a66c4eaefa7b6419d2c43301cf645d0b55
Agustin Chiappe Berrini <agustin@chiappeberrini.me>
2022-04-09 10:15:29 -0700

    nnn support!

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,6 +3,10 @@ if status is-interactive
 end
 alias vw='rdrview -B lynx'
 alias vm='glow -p'
+alias vp='termpdf.py'
+alias nnn='nnn -a -P p -c'
 set -g theme_color_scheme nord
 set -g fish_prompt_pwd_dir_length 0
 setenv EDITOR vim
+setenv NNN_OPENER /home/agustin/.config/nnn/plugins/nuke
+setenv NNN_PLUG "p:preview-tui" 
