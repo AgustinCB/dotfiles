a6bd2d70180e56c47a493ef8d13abe0703c24dcf
Agustin Chiappe Berrini <agustin@chiappeberrini.me>
2022-04-11 15:34:53 -0700

    Add more things to watch from the terminal

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,9 +1,16 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-alias vw='rdrview -B lynx'
+alias vi='viu'
+alias vimage='viu'
 alias vm='glow -p'
+alias vmarkdown='vm'
 alias vp='termpdf.py'
+alias vpdf='vp'
+alias vv='mpv --vo=kitty'
+alias vvideo='vv'
+alias vw='rdrview -B lynx'
+alias vwebsite='vw'
 alias nnn='nnn -a -P p -c'
 set -g theme_color_scheme nord
 set -g fish_prompt_pwd_dir_length 0
